fe2a90c19b993d386d8df65f5a34976d8335bd30
SaorCampos <saor93@gmail.com>
2022-10-10 15:22:03 -0300

    fixed add contact function

--- src/components/Contacts/AddContact/AddContact.js
+++ src/components/Contacts/AddContact/AddContact.js
@@ -8,13 +8,19 @@ const AddContact = () => {
         numero: '',
         cidade: '',
     });
-    const handleFormSubmuit = async(e) =>{
-        let response = await axios.post('http://localhost:8000/contatos',formData);
-        response ? alert('Contato Criado') : alert('Algo deu errado');
+    const handleFormSubmuit = (e) =>{
+        let response = axios.post('http://localhost:8000/contatos',formData);
+        if(response){
+            alert('Contato Adicionado');
+            return;
+        }else{
+            alert('Algo deu errado');
+        }
         setFormData({
             nome: '',
             numero: '',
-            cidade: '',});
+            cidade: '',
+        });
     }
     return (
         <React.Fragment>
@@ -26,25 +32,25 @@ const AddContact = () => {
                         </h1>
                     </div>
                     <form>
-                        <div class="form-group row my-2">
-                            <label for="nome" class="col-form-label">Nome</label>
-                            <div class="col">
-                                <input value={formData.nome} type="text" class="form-control" id="nome" placeholder='Nome' pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" onChange={(e) => setFormData({
+                        <div className="form-group row my-2">
+                            <label for="nome" className="col-form-label">Nome</label>
+                            <div className="col">
+                                <input value={formData.nome} type="text" className="form-control" id="nome" placeholder='Nome' pattern="(?=.*[A-Z])(?=.*[a-z]){2,}" onChange={(e) => setFormData({
                                     ...formData, nome: e.target.value})}/>
                             </div>
                         </div>
-                        <div class="form-group row my-2">
-                            <label for="numero" class="col-form-label">Número</label>
-                            <div class="col">
+                        <div className="form-group row my-2">
+                            <label for="numero" className="col-form-label">Número</label>
+                            <div className="col">
                                 <input value={formData.numero} onChange={(e) => setFormData({
-                                    ...formData, numero: e.target.value})} type="text" class="form-control" id="numero" placeholder="Número" pattern="[0-9]{8,}"/>
+                                    ...formData, numero: e.target.value})} type="text" className="form-control" id="numero" placeholder="Número" pattern="[0-9]{8,}"/>
                             </div>
                         </div>
-                        <div class="form-group row my-2">
-                            <label for="cidade" class=" col-form-label">Cidade</label>
-                            <div class="col">
+                        <div className="form-group row my-2">
+                            <label for="cidade" className=" col-form-label">Cidade</label>
+                            <div className="col">
                                 <input value={formData.cidade} onChange={(e) => setFormData({
-                                    ...formData, cidade: e.target.value})} type="text" class="form-control" id="cidade" placeholder="Cidade" pattern="(?=.*[A-Z])(?=.*[a-z]){3,}"/>
+                                    ...formData, cidade: e.target.value})} type="text" className="form-control" id="cidade" placeholder="Cidade" pattern="(?=.*[A-Z])(?=.*[a-z]){3,}"/>
                             </div>
                         </div>
                         <div className="my-4">
